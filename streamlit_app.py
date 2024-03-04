f5fea6499f6512ae6c0b68c0f23fc3f18bf366bd
tshurlock <61873925+tshurlock@users.noreply.github.com>
2024-03-04 16:55:54 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,8 @@ driver.implicitly_wait(10)  # Wait for up to 10 seconds
 st.write(driver.current_url)
 driver.implicitly_wait(10)  # Wait for up to 10 seconds
 try:
-    table = driver.find_element(By.XPATH, "(//table)[3]")
+    #table = driver.find_element(By.XPATH, "(//table)[3]")
+    table = driver.find_element(By.XPATH, '//*[@id="results"]')
     #table = driver.find_element(By.CLASS_NAME, "sortable")
     #table_element = driver.find_element_by_id("results")
     #tables = driver.find_element(By.ID, "results")
