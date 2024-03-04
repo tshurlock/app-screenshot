8d9e10f182c7730bde63d54c0bf138af72a4bfeb
tshurlock <61873925+tshurlock@users.noreply.github.com>
2024-03-04 16:25:52 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ def get_driver():
 driver = get_driver()
 runner = str(47595)
 ParkrunURL = "https://www.parkrun.org.uk//parkrunner//" + runner + "//all//"
-driver.get("https://www.parkrun.org.uk/parkrunner/47595/all/")
+driver.get("https://www.bbc.co.uk/news/uk-68468019")
 driver.implicitly_wait(10)  # Wait for up to 10 seconds
 try:
     table = driver.find_element(By.XPATH, "(//table)[3]")
