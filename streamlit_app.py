79e916a803286729638aca21e1c0a8caac4c89db
tshurlock <61873925+tshurlock@users.noreply.github.com>
2024-03-04 16:15:25 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,7 @@ def get_driver():
 driver = get_driver()
 runner = str(47595)
 ParkrunURL = "https://www.parkrun.org.uk//parkrunner//" + runner + "//all//"
+driver.get(ParkrunURL)
 driver.implicitly_wait(10)  # Wait for up to 10 seconds
 try:
     table = driver.find_element(By.XPATH, "(//table)[3]")
